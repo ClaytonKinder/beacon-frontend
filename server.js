e94f50287bbbef1240542c0946927bc7d4be4a80
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-10-11 15:35:53 -0400

    Fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static')
 app = express();
+require('dotenv').config({ path: 'variables.env' });
 app.use(serveStatic(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
