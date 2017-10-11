224038201ab9f079a958c82ff41debd75cc1d54b
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-10-11 15:57:45 -0400

    Fix

---
+++ config/dev.env.js
@@ -0,0 +1,10 @@
+require('dotenv').config({ path: 'variables-dev.env' });
+
+module.exports = {
+  NODE_ENV: `'${process.env.NODE_ENV}'`,
+  MAPS_API_KEY: `'${process.env.MAPS_API_KEY}'`,
+  GEOLOCATION_API_KEY: `'${process.env.GEOLOCATION_API_KEY}'`,
+  SITE_URL: `'${process.env.SITE_URL}'`,
+  SERVER_URL: `'${process.env.SERVER_URL}'`,
+  API_URL: `'${process.env.API_URL}'`
+}
