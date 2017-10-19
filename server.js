6a424cf887a28e76d10298a5d85279baa9f23714
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2017-10-19 13:07:14 -0400

    HTTPS fix

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var history = require('connect-history-api-fallback');
 var serveStatic = require('serve-static');
 
 app = express();
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]));
 app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+port);
+console.log('server started ' + port);
